61c8f312dc1f096a8a3fb164e9c468e81da05500
ActualNugget <isvaran@tuta.io>
2026-01-30 22:32:38 +0800

    feat(player): fog of war!

---
+++ scripts/fog.gd
@@ -0,0 +1,42 @@
+extends Node2D
+
+const LightTexture = preload("res://assets/sprites/Light.png")
+const GRID_SIZE = 16
+
+@onready var fog = $Fog
+
+var display_width = ProjectSettings.get("display/window/size/viewport_width")
+var display_height = ProjectSettings.get("display/window/size/viewport_height")
+
+var fogImage = Image.new()
+var fogTexture = ImageTexture.new()
+var lightImage = LightTexture.get_image()
+var light_offset = Vector2(LightTexture.get_width()/2, LightTexture.get_height()/2)
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var fog_image_width = display_width/GRID_SIZE
+	var fog_image_height = display_height/GRID_SIZE
+	fogImage = Image.create_empty(fog_image_width, fog_image_height, false, Image.FORMAT_RGBAH)
+	fogImage.fill(Color.BLACK)
+	lightImage.convert(Image.FORMAT_RGBAH)
+	fog.scale *= GRID_SIZE
+
+func update_fog(new_grid_position):
+	#fogImage.lock()
+	#lightImage.lock()
+	
+	var light_rect = Rect2(Vector2.ZERO, Vector2(lightImage.get_width(), lightImage.get_height()))
+	fogImage.blend_rect(lightImage, light_rect, new_grid_position - light_offset)
+	
+	#fogImage.unlock()
+	#lightImage.unlock()
+	update_fog_image_texture()
+
+func update_fog_image_texture():
+	fogTexture = ImageTexture.create_from_image(fogImage)
+	fog.texture = fogTexture
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	update_fog($"../Player".position/GRID_SIZE)
