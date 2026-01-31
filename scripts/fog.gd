a44dfc6f8b154f39df29d6afdb4276a24ef8ba08
ActualNugget <isvaran@tuta.io>
2026-01-31 21:00:14 +0800

    fix(map):removed grid effect

--- scripts/fog.gd
+++ scripts/fog.gd
@@ -1,10 +1,12 @@
 extends Node2D
 # https://www.youtube.com/watch?v=zkMSbQoUd9o
 
-#const LightTexture = preload("res://assets/sprites/kenney_particle-pack/PNG (Transparent)/circle_05.png")
-const LightTexture = preload("res://assets/sprites/circle_05_64x.png")
-const GRID_SIZE = 16
-@export var LIGHT_SIZE = 30
+# TODO: still has the watercolour ghosting (darker pixels at light edge darken already explored areas as we leave)
+
+
+const LightTexture = preload("res://assets/sprites/circle_05.png")
+
+@export var LIGHT_SIZE = 300
 
 @onready var fog = $Fog
 
@@ -18,21 +20,18 @@ var light_offset = Vector2(LIGHT_SIZE/2, LIGHT_SIZE/2)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var fog_image_width = display_width/GRID_SIZE
-	var fog_image_height = display_height/GRID_SIZE
-	fogImage = Image.create_empty(fog_image_width, fog_image_height, false, Image.FORMAT_RGBAH)
+	fogImage = Image.create_empty(display_width, display_height, false, Image.FORMAT_RGBAH)
 	fogImage.fill(Color.BLACK)
 	lightImage.resize(LIGHT_SIZE, LIGHT_SIZE)
 	lightImage.convert(Image.FORMAT_RGBAH)
-	fog.scale *= GRID_SIZE
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	update_fog($"../Player".position/GRID_SIZE)
+	update_fog($"../Player".position)
 
-func update_fog(new_grid_position):
+func update_fog(light_position):
 	var light_rect = Rect2(Vector2.ZERO, Vector2(LIGHT_SIZE, LIGHT_SIZE))
-	fogImage.blend_rect(lightImage, light_rect, new_grid_position - light_offset)
+	fogImage.blend_rect(lightImage, light_rect, light_position - light_offset)
 	update_fog_image_texture()
 
 func update_fog_image_texture():
